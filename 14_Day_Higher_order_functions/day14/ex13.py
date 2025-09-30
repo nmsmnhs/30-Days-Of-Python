98abcb20b39ad33fa14267510952ee9ebd21f7ce
nmsmnhs <phuongquynh.pham1202@gmail.com>
2025-09-30 12:20:15 +0700

    day 14 done

--- 14_Day_Higher_order_functions/day14/ex13.py
+++ 14_Day_Higher_order_functions/day14/ex13.py
@@ -2,3 +2,10 @@ from countries import countries_list
 
 start = {}
 
+for country in countries_list:
+    if country[0] not in start:
+        start[country[0]] = 1
+    else:
+        start[country[0]] += 1
+
+print(start)
